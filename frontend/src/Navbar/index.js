10ffd37bd1336236bd6b030bcf87adaeac8ad23a
Parvin khan <58837691+parveen11498@users.noreply.github.com>
2022-06-21 16:45:05 +0530

    Add files via upload

---
+++ frontend/src/Navbar/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// import {
+//   ul
+// //   NavLink,
+// //   Bars,
+// //   NavMenu,
+// //   NavBtn,
+// //   NavBtnLink,
+// // } from './NavbarElements';
+// }
+const Navbar = () => {
+return(
+   
+  <ul>
+  <li><a href="#">Home</a></li>
+  <li><a href="#">News</a></li>
+  <li><a href="#">Contact</a></li>
+  <li><a href="#">About</a></li>
+</ul> 
+)
+
+   
+  };
+  
+export default Navbar;
